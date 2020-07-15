2d7b5571155b1f367ba9bbb5fdb3756821d870d8
masarufuruya <masarufuruya116@gmail.com>
2020-07-15 22:39:07 +0900

    戻す

--- config/cors.php
+++ config/cors.php
@@ -31,7 +31,7 @@ return [
     /*
      * Matches the request origin. `[*]` allows all origins.
      */
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('CORS_ALLOWED_ORIGIN')],
 
     /*
      * Matches the request origin with, similar to `Request::is()`
