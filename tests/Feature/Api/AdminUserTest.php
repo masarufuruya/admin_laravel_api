97cf76c02356ffa225c058943e93f26008a1c6b8
masarufuruya <masarufuruya116@gmail.com>
2020-09-20 00:43:23 +0900

    indexメソッドのテストケース追加

--- tests/Feature/Api/AdminUserTest.php
+++ tests/Feature/Api/AdminUserTest.php
@@ -2,21 +2,41 @@
 
 namespace Tests\Feature\Api;
 
+use App\AdminUser;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AdminUserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    private $adminUser;
+
+    public function testIndex()
     {
-        factory(\App\AdminUser::class)->create();
-        // $adminUser = \App\AdminUser::first();
-        // $this->assertNotNull($adminUser);
+        $this->WithoutMiddleware();
+        $adminUser1 = factory(AdminUser::class)->create();
+        $adminUser2 = factory(AdminUser::class)->create();
+
+        $response = $this->json('GET', '/api/admin_users');
+        $response
+            ->assertStatus(200)
+            ->assertJson(
+                [
+                    [
+                        'id' => $adminUser1->id,
+                        'name' => $adminUser1->name,
+                        'email' => $adminUser1->email,
+                    ],
+                    [
+                        'id' => $adminUser2->id,
+                        'name' => $adminUser2->name,
+                        'email' => $adminUser2->email,
+                    ]
+                ]
+            );
     }
 }
